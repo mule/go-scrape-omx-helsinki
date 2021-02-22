15d7105ca2fba914c6f5ba4fb21471c1f78c24ec
Jukka Puranen <jukka.puranen@gmail.com>
2021-02-22 14:42:22 +0200

    kauppalehti scraping done with ferret

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,81 @@
 package main
 
 import (
-	"log"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
 
-	scrapeOMXHelsinki "github.com/mule/go-scrape-omx-helsinki"
+	"github.com/MontFerret/ferret/pkg/compiler"
+	"github.com/MontFerret/ferret/pkg/drivers"
+	"github.com/MontFerret/ferret/pkg/drivers/cdp"
+	"github.com/MontFerret/ferret/pkg/drivers/http"
 )
 
+type Stock struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
 func main() {
+	listedStocks, err := getListedStocks()
 
-	log.Println("Starting OMX Helsinki Scraper")
-	log.Println(scrapeOMXHelsinki.Config())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	for _, s := range listedStocks {
+		fmt.Println(fmt.Sprintf("%s: %s", s.Name, s.URL))
+	}
+}
+
+func getListedStocks() ([]Stock, error) {
+	query := `
+						LET doc = DOCUMENT('https://www.kauppalehti.fi/porssi/indeksit/OMXHPI', {driver: "cdp"})
+						WAIT_ELEMENT(doc, '.stock-list', 5000)
+						LET stockNameLinks = ELEMENTS(doc, '.stock-name-link')
+						FOR stockNameLink IN stockNameLinks
+						LET stockName = stockNameLink.innerText
+						LET link =stockNameLink.attributes.href
+						RETURN {
+							name: stockName,
+							url: link
+						}
+			}
+	`
+
+	comp := compiler.New()
+
+	program, err := comp.Compile(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// create a root context
+	ctx := context.Background()
+
+	// enable HTML drivers
+	// by default, Ferret Runtime does not know about any HTML drivers
+	// all HTML manipulations are done via functions from standard library
+	// that assume that at least one driver is available
+	ctx = drivers.WithContext(ctx, cdp.NewDriver())
+	ctx = drivers.WithContext(ctx, http.NewDriver(), drivers.AsDefault())
+
+	out, err := program.Run(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]Stock, 100)
+
+	err = json.Unmarshal(out, &res)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mule/go-scrape-omx-helsinki
 
 go 1.15
+
+require (
+	astuart.co/goq v1.0.0 // indirect
+	github.com/MontFerret/ferret v0.13.0 // indirect
+)
